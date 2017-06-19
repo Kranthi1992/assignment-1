3e0bffb5e7f2ad00c35b9ff4413089696605039f
Kranthi1992 <kittukranthi18@gmail.com>
2017-06-18 19:11:36 -0700

    Assingment 2

---
+++ src/mypackage/Employeeclass.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+public class Employeeclass {
+	 private String empName;
+	 private int empId;
+	 private double empSal;
+	 Employeeclass employees[] = new Employeeclass[10];
+	
+	 public Employeeclass(String name,int id,double sal){
+		 this.empName = name;
+		 this.empId = id;
+		 this.empSal= Sal;
+     }
+	
+	 public String getEmpName(){
+		 return empName;
+	 }
+	
+	 public int getEmpId(){
+		 return empId;
+	 }
+
+	 public double getSalary(){
+		 return empSal;
+	 }
+
+}
+
---
+++ src/mypackage/Myclass.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package mypackage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Myclass {
+	public static void main(String[] args) {
+		Map<Integer,Employeeclass> empMap=new HashMap<Integer,Employeeclass>();
+		for (int i = 0; i <= 10; i++) {
+			String name="emp" + i;
+			int id= i;
+			double sal = i+100;
+			Employeeclass employee = new Employeeclass(name,id,sal);
+			empMap.put(i, employee);
+		}
+		
+		
+		 for(Map.Entry<Integer, Employeeclass> entry:empMap.entrySet()){ 
+			 	int employeeId=entry.getKey();
+			 	Employeeclass empDetails=entry.getValue();
+			 	System.out.println(employeeId+" Details:");
+
+			 	System.out.println("emp name is");
+		 }
+		 
+		    }   
+	}
+
+
